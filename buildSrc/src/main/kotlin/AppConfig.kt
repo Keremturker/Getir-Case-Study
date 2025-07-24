7d3fc1d5bc2a22a9c80a0097a5857cd8b743d492
keremturker <info@kturker.com>
2025-07-24 12:45:04 +0300

    chore: initialize buildSrc module with centralized AppConfig

---
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -0,0 +1,27 @@
+import org.gradle.api.JavaVersion
+
+object AppConfig {
+    const val applicationId = "com.kturker.getircasestudy"
+    const val name = "Getir Case Study"
+
+    const val compileSdkVersion = 36
+    const val minSdkVersion = 24
+
+    const val targetSdkVersion = 36
+
+    const val versionCode = 1
+
+    private const val versionMajor = 0
+    private const val versionMinor = 0
+    private const val versionPatch = 1
+
+    const val testRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+    val versionName
+        get() = "$versionMajor.$versionMinor.$versionPatch"
+
+    object CompileOptions {
+        val javaSourceCompatibility = JavaVersion.VERSION_11
+        val kotlinJvmTarget: String = JavaVersion.VERSION_11.toString()
+    }
+}
